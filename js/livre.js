26800f748e673d4bc22d9dead0c9fda3c8d6b2c1
Yoann BETTINELLI <yoann1805@gmail.com>
2021-09-15 10:05:00 +0200

    ajout filtre date

--- js/livre.js
+++ js/livre.js
@@ -4,20 +4,46 @@ $(() => {
 
     let mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre","Novembre", "Décembre"];
 
-    // Date en cours
+    // Afficher Date en cours
     let selectedDate = new Date();
+    let dateShowed = document.getElementById("dateShowed");
+    dateShowed.innerHTML = mois[selectedDate.getMonth()] + " " + selectedDate.getFullYear();
 
-    loadTableLines();
+
+    // Gestion des boutons previous et next
+    let prevMonth = document.getElementById("prevMonth");
+    let nextMonth = document.getElementById("nextMonth");
+
+    prevMonth.addEventListener('click', () => {
+        selectedDate = new Date(selectedDate.setMonth(selectedDate.getMonth() - 1))
+        dateShowed.innerHTML = mois[selectedDate.getMonth()] + " " + selectedDate.getFullYear();
+        loadTableLines(selectedDate);
+    })
+
+    nextMonth.addEventListener('click', () => {
+        selectedDate = new Date(selectedDate.setMonth(selectedDate.getMonth() + 1))
+        dateShowed.innerHTML = mois[selectedDate.getMonth()] + " " + selectedDate.getFullYear();
+        loadTableLines(selectedDate);
+    })
+
+    loadTableLines(selectedDate);
 })
 
-loadTableLines = function () {
+loadTableLines = function (date) {
+
+    let mois = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+    let minDate = date.getFullYear() + "-" + mois[date.getMonth()] + "-01"; // 2021-09-01
+    let maxDate = date.getFullYear() + "-" + mois[date.getMonth()] + "-31"; // 2021-09-31
 
     // Charger la BDD
     let Datastore = require('nedb');
     let db = new Datastore({filename: 'data.db', autoload: true});
 
     // Récupérer tout le contenu de la BDD
-    db.find({}, function (err, docs) {
+    // db.find({})
+
+    // Filtre par date (mois sélectionné)
+    db.find({ $and: [{ date: { $gte: minDate }}, { date: { $lte: maxDate }}] }, function (err, docs) {
         let tableRegistre = document.getElementById("tableRegistre");
         let tableRows = tableRegistre.querySelectorAll("thead > tr ");
 
